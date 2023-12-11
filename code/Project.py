01c2817551086b2f798e4abfae97f5af2fcc1769
RaelHuizenga <r.r.huizenga@student.tudelft.nl>
2023-12-11 16:04:19 +0100

    Implement top_hits

--- code/Project.py
+++ code/Project.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def FastTree(Sequences):
     sequenceList = list(Sequences.values())
     n = len(sequenceList)
@@ -33,25 +35,61 @@ def FormProfile(Sequences): # Should be O(nla), a = 4 (alphabet size)
                 T[i] += 1/n
     return A, C, G, T
 
-def TopHits(Sequences):
-    m = int(n**(1/2))
-    topHits = {}
-    return topHits
 
-def BestHits(Sequences):
-    bestHits = {}
-    return bestHits
+def hamming_distance(pattern_1, pattern_2):
+    distance = 0
+    for i in range(len(pattern_1)):
+        if pattern_1[i] != pattern_2[i]:
+            distance +=1
+    return distance
+
+
+def top_hits(seed_sequences, n):
+    top_hits_dict = {}
+    m = int(np.sqrt(n))
+    while len(seed_sequences) > m:
+        print(f'm = {m}')
+        print(f'seedsequences = {len(seed_sequences)}')
+        single_seed_sequence = seed_sequences[np.random.randint(0, len(seed_sequences))]
+        close_neighbors = calculate_close_neighbors(single_seed_sequence, seed_sequences, m)
+        top_hits_helper = calculate_close_neighbors(single_seed_sequence, seed_sequences, 2*m)
+        for neighbor in close_neighbors:
+            top_hits_dict[neighbor] = calculate_close_neighbors(neighbor, top_hits_helper, m)
+            seed_sequences.remove(neighbor)
+    for remaining_sequence in seed_sequences:
+        top_hits_sequences = seed_sequences.copy()
+        top_hits_dict[remaining_sequence] = top_hits_sequences.remove(remaining_sequence)
+    return top_hits_dict
+
+
+def calculate_close_neighbors(seed, sequences, m):
+    if len(sequences) < m:
+        m = len(sequences)
+    hamming_distances = []
+    for i in range(len(sequences)):
+        hamming_distances.append(hamming_distance(seed, sequences[i]))
+    sorted_hamming_distances = hamming_distances.copy()
+    sorted_hamming_distances.sort()
+    m_distance = sorted_hamming_distances[m-1]
+    # calculate r(A) & r(B)
+    return [sequences[i] for i in range(len(sequences)) if hamming_distances[i] > m_distance]
+
+
+def best_hits(sequences):
+    best_hits = {}
+    return best_hits
 
 
 if __name__ == '__main__':
     data = open('../data/test-small.txt', 'r').read().split(">")
-    sequences = {}
+    sequence_dictionary = {}
     for seq in data[1::]:
         label, DNA = seq.splitlines()
-        sequences[label] = DNA
-    
-    
-#formProfile(sequences)
+        sequence_dictionary[label] = DNA
+    n = top_hits(list(sequence_dictionary.values()), len(sequence_dictionary))
+    print(n)
+
+
 
 
 
