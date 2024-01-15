f4fc33bd23d116eea01866ecc6531531f0fde35b
maritverheij <marit11verheij@gmail.com>
2024-01-15 17:13:05 +0100

    newick done

--- code/newick_format.py
+++ code/newick_format.py
@@ -1,12 +1,9 @@
 def newick_format(node_label, all_nodes):
-    print("node_label: ", node_label)
     node = all_nodes[node_label]
-    print("node: ", node)
-    print("node.get_children(): ", node.get_children())
     if not node.get_children():
         return node.get_label()
     children = node.get_children()
     output =  "(" + newick_format(children[0].get_label(), all_nodes) + "," + newick_format(children[1].get_label(), all_nodes) + ")"
-    # if node.get_parent():
-    #     output += ";"
+    if not node.get_parent():
+        output += ";"
     return output
